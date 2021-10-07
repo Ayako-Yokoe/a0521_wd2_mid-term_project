47d40701203fa611de192478d10c8ae5412b96f3
Ayako-Yokoe <niluvsuall@gmail.com>
2021-10-07 13:21:23 -0700

    change images folder

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,65 @@
-function showCart() {
-    const cartInfo = document.getElementById('cart-info')
-    const cart = document.getElementById('cart')
+// show cart
+const cartInfo = document.getElementById('cart-info')
+const cart = document.getElementById('cart')
+cartInfo.addEventListener('click', function() {
+    cart.classList.toggle('show-cart')
+})
 
-    cartInfo.addEventListener('click', function(){
-        cart.classList.toggle('show-cart')
+
+// add to cart
+const cartBtn = document.querySelectorAll('.store-item-icon')
+
+cartBtn.forEach(function(btn){
+    btn.addEventListener('click', function(event){
+        if(event.target.parentElement.classList.contains('store-item-icon')){ 
+
+            console.log(event.target.parentElement.parentElement.parentElement.nextElementSibling.src)
+
+            let path = event.target.parentElement.parentElement.parentElement.nextElementSibling.src 
+
+            let position = path.indexOf('assets') + 6
+
+            console.log(position)
+
+            let partialPath = path.slice(position)
+
+
+            // from here
+            const item = {}
+            item.img = `img-cart${partialPath}`
+            let name = event.target.parentElement.nextElementSibling.
+            children[0].children[0].textContent  
+            item.name = name
+            let price = event.target.parentElement.nextElementSibling.
+            children[0].children[1].textContent
+            let finalPrice = price.slice(1).trim()
+            item.price = finalPrice
+
+            const cartItem = document.createElement('div')
+            cartItem.classList.add('cart-item')
+
+            cartItem.innerHTML = `
+            <img src="${item.img}" alt="">
+            <div class="item-text">
+                <p id="cart-item-title">${item.name}</p>
+                <span>$</span>
+                <span id="cart-item-price" class="cart-item-price">${item.price}</span>
+            </div>
+            <a href="#" id='cart-item-remove' class="cart-item-remove">
+                <i class="fas fa-trash"></i>
+            </a>
+            </div>
+            `
+            // select cart
+            const cart = document.getElementById('cart')
+            const total = document.querySelector('.cart-total-container')
+
+            cart.insertBefore(cartItem, total)
+            alert('item added to the cart')
+        }
     })
-}
 
-function addToCart() {
-    const cartBtn = document.querySelectorAll('.store-item-icon')
 
-    cartBtn.forEach(function(btn){
-        btn.addEventListener('click', function(event){
-            if(event.target.parentElement.classList.contains('store-item-icon')){ 
-                let fullPath = event.target.parentElement.previouElementSiblig.src 
-                let position = fullPath.indexOf('img') + 3
-                let partialPath = fullPath.slice(pos)
-
-                const item = {}
-                item.img = `img-cart${partialPath}`
-                let name = event.target.parentElement.nextElementSibling.
-                children[0].children[0].textContent  
-                item.name = name
-                let price = event.target.parentElement.nextElementSibling.
-                children[0].children[1].textContent
-                let finalPrice = price.slice(1).trim()
-                item.price = finalPrice
-
-                const cartItem = document.createElement('div')
-                cartItem.classList.add('cart-item') // *4
-
-                cartItem.innerHTML = `
-                <img src="${item.img}" alt="">
-                <div class="item-text">
-                  <p id="cart-item-title">${item.name}</p>
-                  <span>$</span>
-                  <span id="cart-item-price" class="cart-item-price">${item.price}</span>
-                </div>
-                <a href="#" id='cart-item-remove' class="cart-item-remove">
-                  <i class="fas fa-trash"></i>
-                </a>
-              </div>
-                `
-             // select cart
-             const cart = document.getElementById('cart')
-             const total = document.querySelector('.cart-total-container')
-
-             cart.insertBefore(cartItem, total)
-             alert('item added to the cart')
-            }
-        })
-    })
-
-    addToCart()
 
     // show totals
     function showTotals(){
@@ -73,6 +80,5 @@ function addToCart() {
         document.getElementById('item-count').textContent = total.length
 
     }
-}
+})
 
-showTotals()
