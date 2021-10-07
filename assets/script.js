935607fb288ddc91ab8df2791edbfd86482de5ae
Ayako-Yokoe <niluvsuall@gmail.com>
2021-10-07 15:19:40 -0700

    added js second time

--- assets/script.js
+++ assets/script.js
@@ -17,29 +17,39 @@ cartBtn.forEach(function(btn){
 
             let path = event.target.parentElement.parentElement.parentElement.nextElementSibling.src 
 
-            let position = path.indexOf('assets') + 6
+            let position = path.indexOf('images') + 6
 
             console.log(position)
 
             let partialPath = path.slice(position)
 
+            console.log(partialPath)
 
-            // from here
+
             const item = {}
-            item.img = `img-cart${partialPath}`
-            let name = event.target.parentElement.nextElementSibling.
-            children[0].children[0].textContent  
+            item.img = `images-cart${partialPath}`
+
+            console.log(item)
+
+            let name = event.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling.textContent      
+
+            console.log(name)
+
             item.name = name
-            let price = event.target.parentElement.nextElementSibling.
-            children[0].children[1].textContent
+
+            let price = event.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling
+            .nextElementSibling.nextElementSibling.textContent
             let finalPrice = price.slice(1).trim()
             item.price = finalPrice
+
+            console.log(finalPrice)
+            console.log(item)
 
             const cartItem = document.createElement('div')
             cartItem.classList.add('cart-item')
 
             cartItem.innerHTML = `
-            <img src="${item.img}" alt="">
+            <img src="/assets/${item.img}" alt="">
             <div class="item-text">
                 <p id="cart-item-title">${item.name}</p>
                 <span>$</span>
@@ -56,29 +66,46 @@ cartBtn.forEach(function(btn){
 
             cart.insertBefore(cartItem, total)
             alert('item added to the cart')
+
+            //const total = []
+            // const items = document.querySelectorAll('.cart-item-price')
+    
+
+            // items.forEach(function(item){
+            //     total.push(parseFloat(item.textContent))
+            // })
+            // const totalMoney = total.reduce(function(total, item) {
+            //     total += item
+            //     return total
+            // }, 0)
+            // const finalMoney = totalMoney.toFixed(2)
+    
+            // document.getElementById('cart-total').textContent = finalMoney
+            // document.getElementsByClassName('.item-total').textContent = finalMoney
+            // document.getElementById('item-count').textContent = total.length
         }
     })
 
-
+})
 
     // show totals
-    function showTotals(){
-        const total = []
-        const items = document.querySelectorAll('.cart-item-price')
+//     function showTotals(){
+//         const total = []
+//         const items = document.querySelectorAll('.cart-item-price')
 
-        items.forEach(function(item){
-            total.push(parseFloat(item.textContent))
-        })
-        const totalMoney = total.reduce(function(total, item) {
-            total += item
-            return total
-        }, 0)
-        const finalMoney = totalMoney.toFixed(2)
+//         items.forEach(function(item){
+//             total.push(parseFloat(item.textContent))
+//         })
+//         const totalMoney = total.reduce(function(total, item) {
+//             total += item
+//             return total
+//         }, 0)
+//         const finalMoney = totalMoney.toFixed(2)
 
-        document.getElementById('cart-total').textContent = finalMoney
-        document.getElementsByClassName('.item-total').textContent = finalMoney
-        document.getElementById('item-count').textContent = total.length
+//         document.getElementById('cart-total').textContent = finalMoney
+//         document.getElementsByClassName('.item-total').textContent = finalMoney
+//         document.getElementById('item-count').textContent = total.length
 
-    }
-})
+//     }
+// })
 
