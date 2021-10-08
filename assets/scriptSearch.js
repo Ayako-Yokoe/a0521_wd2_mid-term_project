9ee59e7edd8d39bbf35f64169cc9b21f66cef18f
Ayako-Yokoe <niluvsuall@gmail.com>
2021-10-08 00:02:41 -0700

    final commit

--- assets/scriptSearch.js
+++ assets/scriptSearch.js
@@ -1,12 +1,3 @@
-
-/* for test */
-let test=document.getElementsByTagName('div');
-let test2=document.getElementsByClassName('gallery-container-right');
-//console.log(test);
-//console.log(test2);
-
-let words ="mini canele minin kelly cactus cement desert potted plant $ 20"
-console.log(words.indexOf("ni"));
 /* 
     SEARCH (filter) 
 */
@@ -19,14 +10,8 @@ searchInput.addEventListener('keyup',function(event){
 
     var productName = galleryContainerRight.getElementsByTagName('h3'); 
     console.log(productName);
-    
-// Array.from() to convert it to an array
-    Array.from(productName).forEach(function(h3){ // <h3></h3>
-        //console.log(h3);
-        //console.log(h3.textContent); // print out all descendant's text
-        //console.log(h3.innerText); // print out direct child's text
-        //console.log(h3.innerHTML);
 
+    Array.from(productName).forEach(function(h3){ 
         let productTitle = h3.textContent.toLowerCase();
         console.log(productTitle.indexOf(searchChar));
 
